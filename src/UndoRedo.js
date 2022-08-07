78cacb6ec20f33aa45360fa713505dce29c486c4
Tigran Aginyan <taginyan91@gmail.comm>
2022-08-07 17:29:33 +0400

    undo/redo functionality improvement

--- src/UndoRedo.js
+++ src/UndoRedo.js
@@ -1,17 +1,34 @@
 import React from "react";
-import { useDispatch } from "react-redux/es/exports";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 import { undo } from "./redux/features/boardsReducerSlice";
 import { redo } from "./redux/features/boardsReducerSlice";
 
 let UndoRedo = () => {
   const dispatch = useDispatch();
 
+  const past = useSelector((state) => state.boards.boards.past);
+  console.log(past);
+  const future = useSelector((state) => state.boards.boards.future);
+  console.log(future);
+
+  const undoHandler = () => {
+    if (past.length !== 0) {
+      dispatch(undo());
+    }
+  };
+
+  const redoHandler = () => {
+    if (future.length !== 0) {
+      dispatch(redo());
+    }
+  };
+
   return (
     <>
-      <button className="undo" onClick={() => dispatch(undo())}>
+      <button className="undo" onClick={undoHandler}>
         Undo
       </button>
-      <button className="redo" onClick={() => dispatch(redo())}>
+      <button className="redo" onClick={redoHandler}>
         Redo
       </button>
     </>
